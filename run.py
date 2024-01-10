0396dc4eba4c78812cdb8681d2d5c7d9b5f2decb
Idehed anton <anton.idehed@telia.com>
2024-01-10 19:01:53 +0000

    creating the name for the function i will use

--- run.py
+++ run.py
@@ -7,6 +7,35 @@ HIDDEN_COMPUTER = [['( )'] *11 for x in range(11)]
 PLAYER_BOARD_SEEN = [['( )'] *11 for x in range(11)]
 
 # Converting Letter to numbers
-LETTER_TO_NUM = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10}
+LETTER_TO_NUM = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
 
+
+def welcome_instruction_name():
+    pass
+
+
+
+def create_board():
+    pass
+
+
+
+def create_ships():
+    pass
+
+
+
+
+def ship_hit():
+    pass
+
+
+
+def get_ships():
+    pass
+
+
+
+def game_battleship():
+    pass
 
