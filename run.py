4ba306bff911bea533d7194a4a2471fd1a2ebc76
Idehed anton <anton.idehed@telia.com>
2024-01-10 20:12:47 +0000

    creating welcome_instruction_name function

--- run.py
+++ run.py
@@ -1,17 +1,34 @@
 import random
 
 # Board for the computer
-HIDDEN_COMPUTER = [['( )'] *11 for x in range(11)]
+HIDDEN_COMPUTER = [[' '] *11 for x in range(11)]
 
 # Board for the user. Here you will se misses and hits on the ships.
-PLAYER_BOARD_SEEN = [['( )'] *11 for x in range(11)]
+PLAYER_BOARD_SEEN = [[' '] *11 for x in range(11)]
 
 # Converting Letter to numbers
 LETTER_TO_NUM = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
 
 
 def welcome_instruction_name():
-    pass
+    '''
+    Welcome message is desplayed and also some information about the game. 
+    Player will start the game with tying in their name.
+    '''
+    print("-----------------------")
+    print("Welcome to Battleships!")
+    print("-----------------------\n")
+    print("-Your task is to sink all 5 boats that are in the ocean")
+    print("-The boats are randomly placed and can be 1-3 in lenght")
+    print("-Guess a row (A - J) and a column (0 - 9)")
+    print("-You will have 10 turns to complete the task")
+    print("-To start the game enter your name\n")
+
+    player_name = input("Please enter your name: ")
+
+    print(f"Let's sink some boats {player_name}!")
+
+welcome_instruction_name()
 
 
 
